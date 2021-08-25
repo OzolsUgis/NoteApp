fcfc7396e3b06ac7c355bf4f993830642288aac5
Ozzy <Ozols.ugiss@gmail.com>
2021-08-25 22:50:47 +0300

    notes entity created

---
+++ app/src/main/java/com/ugisozols/noteapp/data/local/entities/Notes.kt
@@ -0,0 +1,19 @@
+package com.ugisozols.noteapp.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import java.util.*
+
+
+@Entity(tableName = "notes")
+data class Notes(
+    val title : String,
+    val date : Long,
+    val content : String,
+    val userEmail : String,
+    @Expose(deserialize = false, serialize = false)
+    val isSyncedToServer : Boolean,
+    @PrimaryKey
+    val id : String = UUID.randomUUID().toString()
+)
