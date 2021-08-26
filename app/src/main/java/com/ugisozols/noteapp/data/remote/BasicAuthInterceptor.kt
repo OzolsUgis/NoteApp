cf8c884536608f3b4a879a0ee6360977b5809a2f
Ozzy <Ozols.ugiss@gmail.com>
2021-08-26 21:56:17 +0300

    basic auth interceptor created for authorization for api calls who need authentication

---
+++ app/src/main/java/com/ugisozols/noteapp/data/remote/BasicAuthInterceptor.kt
@@ -0,0 +1,28 @@
+package com.ugisozols.noteapp.data.remote
+
+import com.ugisozols.noteapp.utitilies.Constants.BASIC_AUTH_IGNORE_URLS
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class BasicAuthInterceptor : Interceptor {
+
+    private val email : String? = null
+    private val password : String? = null
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        // Because we don't need authentication for /login and / register we
+        // add those end points in Ignore Urls constant and check if encoded
+        // path is in those urls if it is then return request proceed
+        val request = chain.request()
+        if(request.url().encodedPath() in BASIC_AUTH_IGNORE_URLS){
+            return chain.proceed(request)
+        }
+
+        val authenticatedRequest = request.newBuilder()
+                .addHeader("Authorization",
+                Credentials.basic(email?:"", password?:"")
+            ).build()
+        return chain.proceed(authenticatedRequest)
+    }
+}
