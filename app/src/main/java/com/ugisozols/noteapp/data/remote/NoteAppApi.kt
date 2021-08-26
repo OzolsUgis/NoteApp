71184616a88648746586b668682c75d8313a47d3
Ozzy <Ozols.ugiss@gmail.com>
2021-08-26 21:59:27 +0300

    application api interface created with all necessary functions

---
+++ app/src/main/java/com/ugisozols/noteapp/data/remote/NoteAppApi.kt
@@ -0,0 +1,57 @@
+package com.ugisozols.noteapp.data.remote
+
+import com.ugisozols.noteapp.data.local.entities.Folders
+import com.ugisozols.noteapp.data.local.entities.Notes
+import com.ugisozols.noteapp.data.remote.requests.AccountRequest
+import com.ugisozols.noteapp.data.remote.requests.DeleteFolderRequest
+import com.ugisozols.noteapp.data.remote.requests.DeleteNoteRequest
+import com.ugisozols.noteapp.data.remote.responses.MainResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface NoteAppApi {
+
+    @POST("/register")
+    suspend fun register(
+        @Body registerRequest : AccountRequest
+    ): Response<MainResponse>
+
+    @POST("/login")
+    suspend fun login(
+        @Body loginRequest: AccountRequest
+    ):Response<MainResponse>
+
+    @POST("/addNotes")
+    suspend fun addNotes(
+        @Body notes: Notes
+    ):Response<MainResponse>
+
+    @POST("/deleteNote")
+    suspend fun deleteNote(
+        @Body deleteNoteRequest: DeleteNoteRequest
+    ): Response<MainResponse>
+
+    @GET("/getNotes")
+    suspend fun getNotes(): Response<List<Notes>>
+
+    @POST("addFolder")
+    suspend fun addFolder(
+        @Body folders: Folders
+    ): Response<MainResponse>
+
+    @POST("/deleteFolder")
+    suspend fun deleteFolder(
+        @Body deleteFolderRequest: DeleteFolderRequest
+    ): Response<MainResponse>
+
+    @GET("/getFolder")
+    suspend fun getFolder():Response<List<Folders>>
+
+
+
+
+
+
+}
