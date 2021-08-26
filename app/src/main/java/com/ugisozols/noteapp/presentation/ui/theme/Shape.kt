bf7e205d1b6f08d2e9d2b3b2b8ccfd94f32986b7
Ozzy <Ozols.ugiss@gmail.com>
2021-08-26 22:04:57 +0300

    Files refactored to presentation package

--- app/src/main/java/com/ugisozols/noteapp/presentation/ui/theme/Shape.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.ugisozols.noteapp.ui.theme
+package com.ugisozols.noteapp.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
