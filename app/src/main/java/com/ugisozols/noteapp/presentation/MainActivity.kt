83921b68a42ce1806003d598cf2d9cae8e803799
Ozzy <Ozols.ugiss@gmail.com>
2021-08-26 22:27:33 +0300

    main activity annotated with android entry point

--- app/src/main/java/com/ugisozols/noteapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import com.ugisozols.noteapp.presentation.ui.theme.NoteAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
