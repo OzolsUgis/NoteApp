a49a691914ef14df53e9f6651eaa69a240904e66
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 22:13:25 +0300

    Errors & Routes constant for register scvreen added

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Constants.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Constants.kt
@@ -6,4 +6,12 @@ object Constants {
 
     const val DATABASE_NAME = "notes_db"
     const val BASE_URL = "http://10.0.2.2:8000"
+
+    // Errors
+
+    const val SERVER_CONNECTION_ERROR = "Couldn't connect to the server"
+
+    // Routes
+
+    const val REGISTER_SCREEN_ROUTE = "register"
 }
