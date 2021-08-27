bcdfdccc2d7f923a8cece763f5bee1287f03598c
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 23:26:01 +0300

    error constants added

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Constants.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Constants.kt
@@ -6,10 +6,14 @@ object Constants {
 
     const val DATABASE_NAME = "notes_db"
     const val BASE_URL = "http://10.0.2.2:8000"
+    const val MIN_PASSWORD_LENGTH = 8
 
     // Errors
 
-    const val SERVER_CONNECTION_ERROR = "Couldn't connect to the server"
+    const val SERVER_CONNECTION_ERROR = "Couldn't connect to the server!"
+    const val EMPTY_FIELD_ERROR = "Please fill in all fields!"
+    const val TOO_SHORT_PASSWORD_ERROR = "Password is too short !"
+    const val PASSWORDS_DO_NOT_MATCH_ERROR = "Passwords do not match!"
 
     // Routes
 
