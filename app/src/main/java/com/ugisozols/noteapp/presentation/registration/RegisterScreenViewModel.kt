72c9a80ae73fb208c20cb2906d41894183acc85f
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 23:24:37 +0300

    password validation created

--- app/src/main/java/com/ugisozols/noteapp/presentation/registration/RegisterScreenViewModel.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/registration/RegisterScreenViewModel.kt
@@ -1,12 +1,41 @@
 package com.ugisozols.noteapp.presentation.registration
 
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ugisozols.noteapp.repository.AuthRepository
+import com.ugisozols.noteapp.utitilies.Constants.EMPTY_FIELD_ERROR
+import com.ugisozols.noteapp.utitilies.Constants.MIN_PASSWORD_LENGTH
+import com.ugisozols.noteapp.utitilies.Constants.PASSWORDS_DO_NOT_MATCH_ERROR
+import com.ugisozols.noteapp.utitilies.Constants.TOO_SHORT_PASSWORD_ERROR
+import com.ugisozols.noteapp.utitilies.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RegisterScreenViewModel @Inject constructor(
     private val repository : AuthRepository
 ): ViewModel() {
+
+    private val _register = MutableLiveData<Resource<String>>()
+    val register :LiveData<Resource<String>> = _register
+
+    fun registerUser(email : String, password : String, reEnteredPassword : String){
+        _register.postValue(Resource.Loading(null))
+        if(email.isEmpty()||password.isEmpty()||reEnteredPassword.isEmpty()){
+            _register.postValue(Resource.Error(EMPTY_FIELD_ERROR))
+        }
+        if(password.length < MIN_PASSWORD_LENGTH){
+          _register.postValue(Resource.Error(TOO_SHORT_PASSWORD_ERROR))
+        }
+        if(password != reEnteredPassword){
+            _register.postValue(Resource.Error(PASSWORDS_DO_NOT_MATCH_ERROR))
+        }
+        viewModelScope.launch {
+            _register.postValue(repository.register(email,password))
+        }
+    }
 }
