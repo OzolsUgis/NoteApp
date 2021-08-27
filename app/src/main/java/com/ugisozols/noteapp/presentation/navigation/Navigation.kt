c248a1629cb6f7e4b39fd9b267f7690376f3c901
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 23:25:23 +0300

    start destination added

--- app/src/main/java/com/ugisozols/noteapp/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/navigation/Navigation.kt
@@ -5,13 +5,14 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.ugisozols.noteapp.presentation.registration.RegistrationScreen
 import com.ugisozols.noteapp.utitilies.Screen
 
 @Composable
 fun Navigation( navController : NavHostController) {
-    NavHost(navController = navController, startDestination = ""){
+    NavHost(navController = navController, startDestination = Screen.Register.route){
         composable(Screen.Register.route){
-
+            RegistrationScreen()
         }
 
     }
