23686d01956a3ce37122b17eef7e1438ae0cc308
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 23:25:46 +0300

    viewModel initiated

--- app/src/main/java/com/ugisozols/noteapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/MainActivity.kt
@@ -3,12 +3,16 @@ package com.ugisozols.noteapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.ugisozols.noteapp.presentation.navigation.Navigation
+import com.ugisozols.noteapp.presentation.registration.RegisterScreenViewModel
 import com.ugisozols.noteapp.presentation.ui.theme.NoteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -17,6 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NoteAppTheme {
                 val navController = rememberNavController()
+                val registerScreenViewModel : RegisterScreenViewModel by viewModels()
                 Surface(color = MaterialTheme.colors.background) {
                     Navigation(navController = navController)
                 }
