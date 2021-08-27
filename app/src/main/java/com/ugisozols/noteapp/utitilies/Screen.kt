602dc8cdc124b27cbff1211bb2f0cb0d949ce4a4
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 22:14:30 +0300

    register object created

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
@@ -1,5 +1,7 @@
 package com.ugisozols.noteapp.utitilies
 
-sealed class Screen (val route : String){
+import com.ugisozols.noteapp.utitilies.Constants.REGISTER_SCREEN_ROUTE
 
+sealed class Screen (val route : String){
+    object Register : Screen(REGISTER_SCREEN_ROUTE)
 }
