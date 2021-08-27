1f2621e72fbc8f1d669ff39eb3dca4ae5d7654c4
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 22:13:04 +0300

    register User function in repository for efficient response handling

---
+++ app/src/main/java/com/ugisozols/noteapp/repository/AuthRepository.kt
@@ -0,0 +1,28 @@
+package com.ugisozols.noteapp.repository
+
+import com.ugisozols.noteapp.data.remote.NoteAppApi
+import com.ugisozols.noteapp.data.remote.requests.AccountRequest
+import com.ugisozols.noteapp.utitilies.Constants.SERVER_CONNECTION_ERROR
+import com.ugisozols.noteapp.utitilies.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import javax.inject.Inject
+
+
+class AuthRepository @Inject constructor(
+    private val noteAppApi: NoteAppApi
+) {
+    suspend fun registerUser(email : String, password: String) = withContext(Dispatchers.IO){
+        try {
+            val apiCallResponse = noteAppApi.register(AccountRequest(email, password))
+            if(apiCallResponse.isSuccessful){
+                Resource.Success(apiCallResponse.body())
+            }else{
+                Resource.Error(apiCallResponse.message(), null)
+            }
+        }catch (e : Exception){
+            Resource.Error(SERVER_CONNECTION_ERROR, null)
+        }
+    }
+}
