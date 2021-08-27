1aeb7e292b208c986cfd87167c3f8ee456cd5e54
Ozzy <Ozols.ugiss@gmail.com>
2021-08-27 22:13:55 +0300

    register screen rote added to NavHost Composable

--- app/src/main/java/com/ugisozols/noteapp/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/navigation/Navigation.kt
@@ -5,11 +5,12 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.ugisozols.noteapp.utitilies.Screen
 
 @Composable
 fun Navigation( navController : NavHostController) {
     NavHost(navController = navController, startDestination = ""){
-        composable(""){
+        composable(Screen.Register.route){
 
         }
 
