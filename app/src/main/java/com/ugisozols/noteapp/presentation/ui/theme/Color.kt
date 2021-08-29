dcf5153d7718dbb425b6da98f093cb78f5184bd8
Ozzy <Ozols.ugiss@gmail.com>
2021-08-30 00:17:57 +0300

    surface and button text colors edited

--- app/src/main/java/com/ugisozols/noteapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/ui/theme/Color.kt
@@ -6,5 +6,5 @@ import androidx.compose.ui.graphics.Color
 val BackgroundColor = Color(0xFF121212)
 val MainAccent = Color(0xFFFFC68A)
 val TextColor = Color(0xFFFFC68A)
-val SurfaceColor = Color(0xFF707070)
-val ButtonTextColor = Color(0xFF707070)
+val SurfaceColor = Color(0xFF363636)
+val ButtonTextColor = Color(0xFF363636)
