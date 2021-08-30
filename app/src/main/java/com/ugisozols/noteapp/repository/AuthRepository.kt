8e34ccc65c707838c0fc71b89e2cad8add865d19
Ozzy <Ozols.ugiss@gmail.com>
2021-08-30 22:05:04 +0300

    1 second delay and error logger added

--- app/src/main/java/com/ugisozols/noteapp/repository/AuthRepository.kt
+++ app/src/main/java/com/ugisozols/noteapp/repository/AuthRepository.kt
@@ -5,7 +5,9 @@ import com.ugisozols.noteapp.data.remote.requests.AccountRequest
 import com.ugisozols.noteapp.utitilies.Constants.SERVER_CONNECTION_ERROR
 import com.ugisozols.noteapp.utitilies.Resource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
+import timber.log.Timber
 import java.lang.Exception
 import javax.inject.Inject
 
@@ -17,6 +19,7 @@ class AuthRepository @Inject constructor(
         email: String,
         password: String
     ): Resource<String> = withContext(Dispatchers.IO){
+        delay(1000L)
         try {
             val apiCallResponse = noteAppApi.register(AccountRequest(email, password))
             if(apiCallResponse.isSuccessful){
@@ -25,6 +28,7 @@ class AuthRepository @Inject constructor(
                 Resource.Error(apiCallResponse.message(), null)
             }
         }catch (e : Exception){
+            Timber.d(e)
             Resource.Error(SERVER_CONNECTION_ERROR, null)
         }
     }
