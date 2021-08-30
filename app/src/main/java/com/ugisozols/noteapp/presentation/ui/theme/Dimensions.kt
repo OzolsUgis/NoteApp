aa8851c40b8569b6ae183b97a4a68ddac7346379
Ozzy <Ozols.ugiss@gmail.com>
2021-08-30 22:04:14 +0300

    dimensions for states

--- app/src/main/java/com/ugisozols/noteapp/presentation/ui/theme/Dimensions.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/ui/theme/Dimensions.kt
@@ -1,9 +1,12 @@
 package com.ugisozols.noteapp.presentation.ui.theme
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 val paddingSmall = 8.dp
 val paddingMedium = 16.dp
 val paddingLarge = 24.dp
 
 val textfieldRaundedCorners = 15.dp
+val loadingProgressBarWidth = 3.dp
+val errorFontSize = 14.sp
