8cc06106e93946e989390aa74d021e25a96c2ad7
Ozzy <Ozols.ugiss@gmail.com>
2021-08-31 22:40:23 +0300

    Inheritance error fixed

--- app/src/main/java/com/ugisozols/noteapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/login/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.ugisozols.noteapp.presentation.login
 
+import androidx.lifecycle.ViewModel
 import com.ugisozols.noteapp.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -7,5 +8,5 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val repository: AuthRepository
-) {
+): ViewModel() {
 }
