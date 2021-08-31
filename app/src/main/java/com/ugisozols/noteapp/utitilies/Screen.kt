d04c6e6e443e73a96b3f12268717d5b89e9bff88
Ozzy <Ozols.ugiss@gmail.com>
2021-08-31 22:32:17 +0300

    Login object created

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
@@ -1,7 +1,9 @@
 package com.ugisozols.noteapp.utitilies
 
+import com.ugisozols.noteapp.utitilies.Constants.LOGIN_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.REGISTER_SCREEN_ROUTE
 
 sealed class Screen (val route : String){
     object Register : Screen(REGISTER_SCREEN_ROUTE)
+    object Login : Screen(LOGIN_SCREEN_ROUTE)
 }
