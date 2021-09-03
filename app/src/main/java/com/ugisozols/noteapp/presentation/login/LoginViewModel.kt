8cddb3fb8cfa8b5e56415e13d413dd65411c0044
Ozzy <Ozols.ugiss@gmail.com>
2021-09-03 21:27:34 +0300

    login view model created

--- app/src/main/java/com/ugisozols/noteapp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/login/LoginViewModel.kt
@@ -1,12 +1,48 @@
 package com.ugisozols.noteapp.presentation.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.ugisozols.noteapp.repository.AuthRepository
+import com.ugisozols.noteapp.utitilies.Constants
+import com.ugisozols.noteapp.utitilies.Constants.EMPTY_FIELD_ERROR
+import com.ugisozols.noteapp.utitilies.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val repository: AuthRepository
 ): ViewModel() {
+
+
+    private val _email = MutableLiveData<String>("")
+    val email : LiveData<String> = _email
+
+    fun onEmailChange(newEmail : String){
+        _email.value = newEmail
+    }
+
+
+    private val _password = MutableLiveData<String>("")
+    val password : LiveData<String> = _password
+
+    fun onPasswordChange(newPassword: String){
+        _password.value = newPassword
+    }
+
+    private val _login = MutableLiveData<Resource<String>>()
+    val login : LiveData<Resource<String>> = _login
+
+    fun login(email : String, password: String){
+        _login.postValue(Resource.Loading(null))
+        if(email.isEmpty()||password.isEmpty()){
+            _login.postValue( Resource.Error(EMPTY_FIELD_ERROR))
+        }
+        viewModelScope.launch {
+            _login.postValue(repository.login(email, password))
+        }
+    }
 }
