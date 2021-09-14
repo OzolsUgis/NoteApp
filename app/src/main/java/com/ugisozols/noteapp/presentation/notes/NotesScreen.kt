5fb8e2e2c5c9d55d9f0ebfb55c95c84648e64b09
Ozzy <Ozols.ugiss@gmail.com>
2021-09-14 21:14:06 +0300

    notes screen nav bar implemented and get notes works

--- app/src/main/java/com/ugisozols/noteapp/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/notes/NotesScreen.kt
@@ -4,17 +4,33 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.ugisozols.noteapp.R
 import com.ugisozols.noteapp.data.local.entities.Notes
+import com.ugisozols.noteapp.presentation.components.BottomNavBar
+import com.ugisozols.noteapp.presentation.navigation.Navigation
 import com.ugisozols.noteapp.presentation.ui.theme.MainAccent
 import com.ugisozols.noteapp.presentation.ui.theme.loadingProgressBarWidth
+import com.ugisozols.noteapp.utitilies.Constants
+import com.ugisozols.noteapp.utitilies.Constants.LOGIN_SCREEN_ROUTE
+import com.ugisozols.noteapp.utitilies.Constants.NOTES_SCREEN_ROUTE
+import com.ugisozols.noteapp.utitilies.Constants.REGISTER_SCREEN_ROUTE
+import com.ugisozols.noteapp.utitilies.NavBarItem
 import com.ugisozols.noteapp.utitilies.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collect
@@ -25,57 +41,81 @@ import timber.log.Timber
 fun NoteScreen(
     viewModel: NotesViewModel = hiltViewModel()
 ) {
+    ShowNotes(viewModel = viewModel)
     Box(
         modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.BottomStart
     ) {
-        Notes(viewModel = viewModel)
-    }
-}
-
-@Composable
-fun Notes(
-    viewModel: NotesViewModel
-) {
-    val notesDataState = viewModel.notes.observeAsState()
-    val listIsEmpty = notesDataState.value?.data?.isEmpty() ?: return Unit
-    when(notesDataState.value){
-        is Resource.Loading -> {
-            CircularProgressIndicator(
-                color = MainAccent,
-                strokeWidth = loadingProgressBarWidth,
-                modifier = Modifier
-                    .size(100.dp)
-                    .wrapContentSize()
+        val navController = rememberNavController()
+            BottomNavBar(
+                items = listOf(
+                    NavBarItem(
+                        title = stringResource(id = R.string.nav_notes_title),
+                        icon = Icons.Default.Favorite,
+                        route = NOTES_SCREEN_ROUTE
+                    ),
+                    NavBarItem(
+                        title = stringResource(id = R.string.nav_notes_title),
+                        icon = Icons.Default.Favorite,
+                        route = REGISTER_SCREEN_ROUTE
+                    )
+                ),
+                navController = navController,
+                onItemClick = {
+                    navController.navigate(it.route)
+                }
             )
         }
-        is Resource.Success -> {
-            if(listIsEmpty){
-                EmptyState()
-            }else{
-                NotEmptyState()
+
+
+}
+
+
+
+    @Composable
+    fun ShowNotes(
+        viewModel: NotesViewModel
+    ) {
+        val notesDataState = viewModel.notes.observeAsState()
+        val listIsEmpty = notesDataState.value?.data?.isEmpty() ?: return Unit
+        when (notesDataState.value) {
+            is Resource.Loading -> {
+                CircularProgressIndicator(
+                    color = MainAccent,
+                    strokeWidth = loadingProgressBarWidth,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .wrapContentSize()
+                )
+            }
+            is Resource.Success -> {
+                if (listIsEmpty) {
+                    EmptyState()
+                } else {
+                    Text(text = "There are ${notesDataState.value?.data?.lastIndex.toString()} notes")
+                }
+            }
+            is Resource.Error -> {
+                ErrorState()
             }
         }
-        is Resource.Error -> {
-            ErrorState()
-        }
+
     }
 
-}
 
-@Composable
-fun EmptyState() {
-    Text(text = "This is when empty list")
-}
-
-
-@Composable
-fun NotEmptyState() {
-    Text(text = "This is when there are some list items")
-}
+    @Composable
+    fun EmptyState() {
+        Text(text = "This is when empty list")
+    }
 
 
-@Composable
-fun ErrorState() {
-    Text(text = "This is when Error state ")
-}
+    @Composable
+    fun NotEmptyState() {
+        Text(text = "This is when there are some list items")
+    }
+
+
+    @Composable
+    fun ErrorState() {
+        Text(text = "This is when Error state ")
+    }
