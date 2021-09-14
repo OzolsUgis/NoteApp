d7d9eec3eaf02dd9c918ce0b3aedac777e79e13e
Ozzy <Ozols.ugiss@gmail.com>
2021-09-14 21:14:22 +0300

    notes screen object created

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
@@ -1,9 +1,11 @@
 package com.ugisozols.noteapp.utitilies
 
 import com.ugisozols.noteapp.utitilies.Constants.LOGIN_SCREEN_ROUTE
+import com.ugisozols.noteapp.utitilies.Constants.NOTES_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.REGISTER_SCREEN_ROUTE
 
 sealed class Screen (val route : String){
     object Register : Screen(REGISTER_SCREEN_ROUTE)
     object Login : Screen(LOGIN_SCREEN_ROUTE)
+    object Notes : Screen(NOTES_SCREEN_ROUTE)
 }
