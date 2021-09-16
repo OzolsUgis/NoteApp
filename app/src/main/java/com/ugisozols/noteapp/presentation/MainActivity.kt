be03eef27722bb669ddd8e86e482225f909abb40
Ozzy <Ozols.ugiss@gmail.com>
2021-09-16 16:59:12 +0300

    Navigation set

--- app/src/main/java/com/ugisozols/noteapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ugisozols/noteapp/presentation/MainActivity.kt
@@ -3,17 +3,13 @@ package com.ugisozols.noteapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.ugisozols.noteapp.presentation.navigation.Navigation
-import com.ugisozols.noteapp.presentation.notes.NoteScreen
-import com.ugisozols.noteapp.presentation.registration.RegisterScreenViewModel
 import com.ugisozols.noteapp.presentation.ui.theme.NoteAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
+
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
