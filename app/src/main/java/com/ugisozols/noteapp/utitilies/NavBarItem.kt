910bf0d8c8dfb2dc40fa44f48d3e991a1c05b971
Ozzy <Ozols.ugiss@gmail.com>
2021-09-16 16:58:19 +0300

    image vector data type switched to painter

--- app/src/main/java/com/ugisozols/noteapp/utitilies/NavBarItem.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/NavBarItem.kt
@@ -6,5 +6,5 @@ import androidx.compose.ui.graphics.vector.ImageVector
 data class NavBarItem(
     val title : String,
     val route : String,
-    val icon : ImageVector
+    val icon : Painter
     )
