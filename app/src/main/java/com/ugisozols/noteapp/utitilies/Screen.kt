ec0050d3c8031d10819efdb54c120adec4b20e5e
Ozzy <Ozols.ugiss@gmail.com>
2021-09-20 18:19:37 +0300

    new notes screen object created

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
@@ -1,6 +1,7 @@
 package com.ugisozols.noteapp.utitilies
 
 import com.ugisozols.noteapp.utitilies.Constants.LOGIN_SCREEN_ROUTE
+import com.ugisozols.noteapp.utitilies.Constants.NEW_NOTES_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.NOTES_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.REGISTER_SCREEN_ROUTE
 
@@ -8,4 +9,5 @@ sealed class Screen (val route : String){
     object Register : Screen(REGISTER_SCREEN_ROUTE)
     object Login : Screen(LOGIN_SCREEN_ROUTE)
     object Notes : Screen(NOTES_SCREEN_ROUTE)
+    object NewNotes : Screen(NEW_NOTES_SCREEN_ROUTE)
 }
