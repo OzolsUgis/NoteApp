61f92e7061c1c142bedd172d01e8277190da1652
Ozzy <Ozols.ugiss@gmail.com>
2021-09-21 01:42:17 +0300

    nav args function created for passing note Id

--- app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
+++ app/src/main/java/com/ugisozols/noteapp/utitilies/Screen.kt
@@ -1,8 +1,10 @@
 package com.ugisozols.noteapp.utitilies
 
+import com.ugisozols.noteapp.utitilies.Constants.EDIT_NOTE_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.LOGIN_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.NEW_NOTES_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.NOTES_SCREEN_ROUTE
+import com.ugisozols.noteapp.utitilies.Constants.NOTE_DETAIL_SCREEN_ROUTE
 import com.ugisozols.noteapp.utitilies.Constants.REGISTER_SCREEN_ROUTE
 
 sealed class Screen (val route : String){
@@ -10,4 +12,15 @@ sealed class Screen (val route : String){
     object Login : Screen(LOGIN_SCREEN_ROUTE)
     object Notes : Screen(NOTES_SCREEN_ROUTE)
     object NewNotes : Screen(NEW_NOTES_SCREEN_ROUTE)
+    object NoteDetail : Screen(NOTE_DETAIL_SCREEN_ROUTE)
+    object EditNote : Screen(EDIT_NOTE_SCREEN_ROUTE)
+
+    fun withArgs(vararg args : String) :String{
+        return buildString {
+            append(route)
+            args.forEach {
+                append("/$it")
+            }
+        }
+    }
 }
